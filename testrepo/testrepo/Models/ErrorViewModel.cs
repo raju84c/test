325e08da934304c3a459c99cb522a1379bdc8526
raju84c <153365933+raju84c@users.noreply.github.com>
2023-12-10 11:36:09 +0530

    Delete testrepo directory

--- testrepo/testrepo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace testrepo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
